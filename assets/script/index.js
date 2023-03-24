b58518822f692cbdd6943457fcbb1a8bab875c8c
Adil Seco <adil.seco@outlook.com>
2023-03-24 22:23:26 +0000

    added function for current weather and day 1 of forecast

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,8 @@
 var cityList = []
 var day = moment().format("D/M/YYYY")
 
-function getInfo() {
+function getInfo() 
+{
     var city = $("#search-input").val().trim();
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=836d5aa760eed71ca3c64381a74cc400"
        // console.log(city)
@@ -10,3 +11,23 @@ function getInfo() {
             method: "GET"
         }).then(function(response){
               console.log(response)
+
+
+            
+        //weather today
+        var tempC = response.list[0].main.temp - 273.15 
+          $(".cityName").text(response.city.name + " (" + day + ")")
+          $(".temperature").text("Temp: " + tempC.toFixed(2) + "C")
+          $(".wind").text("Wind: " + response.list[0].wind.speed + "KPH")
+          $(".humidity").text("Humidity: " + response.list[0].main.humidity + "%")
+
+        //weather forecast 
+
+        //weather forecast day 1
+          var tomorrow = moment().add(1, 'd').format("D/M/YYYY")
+          //console.log(tomorrow)
+          var tempTomorrowC = response.list[3].main.temp - 273.15 
+          $(".tomorrow").text(tomorrow);
+          $(".tomorrowTem").text("Temp: " + tempTomorrowC.toFixed(2) + "C")
+          $(".tomorrowWind").text("Wind: " + response.list[3].wind.speed + "KPH")
+          $(".tomorrowHum").text("Humidity: " + response.list[3].main.humidity + "%")
