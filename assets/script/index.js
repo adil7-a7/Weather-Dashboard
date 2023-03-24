cb85748450a6d61befd4f94e7945e89369846f42
Adil Seco <adil.seco@outlook.com>
2023-03-24 22:26:51 +0000

    added forecast for 4 other days

--- assets/script/index.js
+++ assets/script/index.js
@@ -16,18 +16,67 @@ function getInfo()
             
         //weather today
         var tempC = response.list[0].main.temp - 273.15 
-          $(".cityName").text(response.city.name + " (" + day + ")")
-          $(".temperature").text("Temp: " + tempC.toFixed(2) + "C")
-          $(".wind").text("Wind: " + response.list[0].wind.speed + "KPH")
-          $(".humidity").text("Humidity: " + response.list[0].main.humidity + "%")
+          $(".cityName").text(response.city.name + " (" + day + ")");
+          $(".temperature").text("Temp: " + tempC.toFixed(2) + "C");
+          $(".wind").text("Wind: " + response.list[0].wind.speed + "KPH");
+          $(".humidity").text("Humidity: " + response.list[0].main.humidity + "%");
 
         //weather forecast 
 
         //weather forecast day 1
-          var tomorrow = moment().add(1, 'd').format("D/M/YYYY")
+          var tomorrow = moment().add(1, 'd').format("D/M/YYYY");
           //console.log(tomorrow)
           var tempTomorrowC = response.list[3].main.temp - 273.15 
           $(".tomorrow").text(tomorrow);
-          $(".tomorrowTem").text("Temp: " + tempTomorrowC.toFixed(2) + "C")
-          $(".tomorrowWind").text("Wind: " + response.list[3].wind.speed + "KPH")
-          $(".tomorrowHum").text("Humidity: " + response.list[3].main.humidity + "%")
+          $(".tomorrowTem").text("Temp: " + tempTomorrowC.toFixed(2) + "C");
+          $(".tomorrowWind").text("Wind: " + response.list[3].wind.speed + "KPH");
+          $(".tomorrowHum").text("Humidity: " + response.list[3].main.humidity + "%");
+
+
+           //City information for two days    
+           var twoDays = moment().add(2, 'd').format("D/M/YYYY")
+           var tempTwoDaysC = response.list[15].main.temp - 273.15 
+ 
+           $(".twoDays").text(twoDays);
+           $(".twoDaysTem").text("Temp: " + tempTwoDaysC.toFixed(2) + "C");
+           $(".twoDaysWind").text("Wind: " + response.list[15].wind.speed + "KPH");
+         $(".twoDaysHum").text("Humidity: " + response.list[15].main.humidity + "%");
+         
+         // forecast day 3
+ var threeDays = moment().add(3, 'd').format("D/M/YYYY")
+ var tempthreeDaysC = response.list[23].main.temp - 273.15 
+ 
+ $(".threeDays").text(threeDays);
+ $(".threeDaysTem").text("Temp: " + tempthreeDaysC.toFixed(2) + "C")
+ $(".threeDaysWind").text("Wind: " + response.list[23].wind.speed + "KPH")
+ $(".threeDaysHum").text("Humidity: " + response.list[23].main.humidity + "%")
+
+ //forecast day 4
+ var fourDays = moment().add(4, 'd').format("D/M/YYYY")
+          var tempFourDaysC = response.list[31].main.temp - 273.15 
+          
+          $(".fourDays").text(fourDays);
+          $(".fourDaysTem").text("Temp: " + tempFourDaysC.toFixed(2) + "C")
+          $(".fourDaysWind").text("Wind: " + response.list[31].wind.speed + "KPH")
+          $(".fourDaysHum").text("Humidity: " + response.list[31].main.humidity + "%")
+
+ //forecast day 5      
+ var fiveDays = moment().add(2, 'd').format("D/M/YYYY")
+ var tempTwoDaysC = response.list[39].main.temp - 273.15 
+ 
+ $(".fiveDays").text(fiveDays);
+ $(".fiveDaysTem").text("Temp: " + tempTwoDaysC.toFixed(2) + "C");
+ $(".fiveDaysWind").text("Wind: " + response.list[39].wind.speed + "KPH");
+ $(".fiveDaysHum").text("Humidity: " + response.list[39].main.humidity + "%");
+
+ // Getting the icon
+ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=836d5aa760eed71ca3c64381a74cc400", function(data) {
+    var iconCode = data.weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/w/"  + iconCode + ".png" ;
+  
+    $(".icon").attr("src", iconURL);
+  });    
+        
+        
+    })
+}
